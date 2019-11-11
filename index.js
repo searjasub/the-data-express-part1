39aaa568b57e47eefae7cbce8b487e752468942a
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-11-11 09:20:52 -0700

    Adding code to connect to mongo db and use cookies

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const routes = require('./routes/routes.js');
 const bcrypt = require('bcrypt-nodejs');
+const cookieParser = require('cookie-parser');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+let mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(cookieParser('We like cookies'));
 
 const urlencodedParser = bodyParser.urlencoded({extended:false});
 
