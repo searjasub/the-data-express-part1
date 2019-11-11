c0c5001d6d4b3961e6a93f01c61d5d56e2b9d7e2
Searjasub Lopez <searjasub.lopez@outlook.com>
2019-11-11 09:06:53 -0700

    Adding initial files

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressSession = require('express-session');
+const pug = require('pug');
+const bodyParser = require('body-parser');
+const path = require('path');
+const routes = require('./routes/routes.js');
+const bcrypt = require('bcrypt-nodejs');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+const urlencodedParser = bodyParser.urlencoded({extended:false});
+
+app.listen(3001);
