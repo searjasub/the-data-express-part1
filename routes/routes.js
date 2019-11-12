5528ff7c1fc0f9067954838e4ee7c47b1c04ed9e
Manny <mannypein1@hotmail.com>
2019-11-12 08:29:27 -0700

    Routes

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+const config = require('/config');
+
+exports.create = (req, res) => {
+    res.render('create',{
+        "title": "Create",
+        "config": config
+    });
+}
+
+exports.login = (req, res) => {
+    res.render('login',{
+        "title": "Login",
+            "config": config
+    });
+}
